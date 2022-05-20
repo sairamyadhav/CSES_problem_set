f9a88537f0e50363fb78baa73c15d1c6f5caf8a2
sairamyadhav <84068020+sairamyadhav@users.noreply.github.com>
2022-05-20 20:20:40 +0530

    Update Number_Spiral.py

--- Number_Spiral.py
+++ Number_Spiral.py
@@ -1,25 +1,11 @@
-r, c, cnt = 4, 2, 0
+r, c = 4, 2
 if r > c:
-    ans = 0
-    cnt = 1
-    for i in range(r):
-        ans += cnt
-        cnt += 2
     if r % 2 == 0:
-        print(ans - c + 1)
+        print(r * r - c + 1)
     else:
-        cnt -= 2
-        ans -= cnt
-        print(ans + c)
+        print((r - 1) * (r - 1) + c)
 else:
-    ans = 0
-    cnt = 1
-    for i in range(c):
-        ans += cnt
-        cnt += 2
     if c % 2 == 1:
-        print(ans - r + 1)
+        print(c * c - r + 1)
     else:
-        cnt -= 2
-        ans -= cnt
-        print(ans + r)
+        print((c - 1) * (c - 1) + r)
